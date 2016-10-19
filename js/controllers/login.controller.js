bbfda4b59780084febb54f6169cfed37d0ab1442
Sandeep J <jagata.sandeep@gmail.com>
2016-10-19 17:49:12 -0400

    Create login.controller.js

---
+++ js/controllers/login.controller.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+	angular.module('demoApp')
+	.controller('Login', Login);
+
+	Login.$inject = ['$rootScope','$scope', 'userService', '$state'];
+
+	function Login( $rootScope, $scope, userService, $state) {
+		var login = this;
+
+    login.login = function (cred) {
+        var user = userService.login(cred);
+
+        if (angular.isUndefined(user)) {
+            alert('username or password incorrect.')
+        } else {
+            $state.go('root.restricted');
+        }
+    };
+  }
+
+})();
